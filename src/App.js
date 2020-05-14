89d842452372776dd33062b131fe7d7822a3c302
susanwere <blue.were@yahoo.com>
2020-05-14 11:51:24 +0300

    Create Signup Page

--- src/App.js
+++ src/App.js
@@ -2,15 +2,34 @@ import React from 'react';
 import './App.scss';
 import NavbarComponent from './components/navbar/navbar';
 import Home from './components/homepage/home';
+import Signup from './components/signup/signup';
+import Login from './components/login/login';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <div className="App">
       <NavbarComponent />
       <header className="App-header">
-          <Home />
+        <Switch>
+          <Route path='/signup'>
+            <Signup />
+          </Route>
+          <Route path='/login'>
+            <Login />
+          </Route>
+          <Route path='/'>
+            <Home />
+          </Route>
+        </Switch>
       </header>
     </div>
+    </Router>
   );
 }
 
